4d5c39122dbca96f86c6b7c33d3f2e1882e360e9
luhclalala <1395324974@qq.com>
2025-03-22 09:26:15 +0800

    update

--- algo/config.py
+++ algo/config.py
@@ -6,7 +6,7 @@ sys.path.append(str(here()))
 class Settings(BaseSettings):
     redis_host: str = "redis"
     mysql_host: str = "mysql"
-    mysql_url: str = "mysql+aiomysql://appuser:apppass@mysql/appdb"
+    mysql_url: str = "mysql+asyncmy://appuser:apppass@mysql/appdb"
     cors_origins: list[str] = ["*"]
 
 settings = Settings() 
